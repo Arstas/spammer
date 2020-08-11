3f18407e0c7799b6e92087634e45cf80704f30c3
palkamilka <68155972+palkamilka@users.noreply.github.com>
2020-08-11 13:27:01 +0300

    Delete wowworks.py

--- wowworks.py
+++
@@ -1,9 +0,0 @@
-from b0mb3r.services.service import Service
-
-
-class WOWWOrks(Service):
-    async def run(self):
-        await self.post(
-            "https://api.wowworks.ru/v2/site/send-code",
-            json={"phone": phone, "type": 2},
-        )
