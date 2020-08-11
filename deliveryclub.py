dcde4d9c2d7bf33468c4f42e245a17dc51771568
palkamilka <68155972+palkamilka@users.noreply.github.com>
2020-08-11 13:24:18 +0300

    Delete deliveryclub.py

--- deliveryclub.py
+++
@@ -1,9 +0,0 @@
-from b0mb3r.services.service import Service
-
-
-class DeliveryClub(Service):
-    async def run(self):
-        await self.post(
-            "https://www.delivery-club.ru/ajax/user_otp",
-            data={"phone": phone},
-        )
